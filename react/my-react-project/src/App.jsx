cf781469605d109081a64db69fc310b4676723c7
Asile Ayuba <asileayuba@gmail.com>
2025-04-10 13:30:38 +0100

    Day 100: Working React for my Stock Prediction Portal

--- react/my-react-project/src/App.jsx
+++ react/my-react-project/src/App.jsx
@@ -1,11 +1,15 @@
-
-
+import HelloWorld from "./components/HelloWorld"
+import LearnReact from "./components/LearnReact"
+import LearnJSX from "./components/LearnJSX"
 
 function App() {
 
   return (
     <>
-      <h1>Learn React</h1>
+      <h1>App Component</h1>
+      {/* <HelloWorld /> */}
+      {/* <LearnReact /> */}
+      <LearnJSX />
     </>
   )
 }
