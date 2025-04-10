abc1134415e4bc163c0c4a467cc0356063aa997c
Asile Ayuba <asileayuba@gmail.com>
2025-04-10 19:19:15 +0100

    Day 100: Working React for my Stock Prediction Portal

--- react/my-react-project/src/components/LearnEvent.jsx
+++ react/my-react-project/src/components/LearnEvent.jsx
@@ -1,8 +1,20 @@
 
 
 const LearnEvent = () => {
+    const handleClick = () => {
+        console.log('Button clicked')
+    }
+
+    const handleClickAgain = (param) => {
+        console.log(param)
+    }
+
   return (
-    <div>LearnEvent</div>
+    <>
+        <button onClick={handleClick}>Click here</button>
+        <br />
+        <button onClick={() => handleClickAgain("Clicked again")}>Click again</button>
+    </>
   )
 }
 
