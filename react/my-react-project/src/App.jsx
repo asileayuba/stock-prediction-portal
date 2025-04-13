d896e952f12dfe5f6d4f134e90ae64fe8c97a61e
Asile Ayuba <asileayuba@gmail.com>
2025-04-13 18:31:38 +0100

    React: CounterApp

--- react/my-react-project/src/App.jsx
+++ react/my-react-project/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <>
-      <h1>App Component</h1>
+      {/* <h1>App Component</h1> */}
       {/* <HelloWorld /> */}
       {/* <LearnReact /> */}
       {/* <LearnJSX /> */}
--- react/my-react-project/src/components/CounterApp.jsx
+++ react/my-react-project/src/components/CounterApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const CounterApp = () => {
+    const [count, setCount] = useState(0)
   return (
     <>
         <h1>CounterApp</h1>
         <h2>Counter:</h2>
+        <button onClick={increaseCount}>Increase Count</button>
     </>
   )
 }
