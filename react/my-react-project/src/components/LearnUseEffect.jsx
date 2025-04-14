8747a3fcf531f27a7bd6c6432c5b0ae15a130f2c
Asile Ayuba <asileayuba@gmail.com>
2025-04-14 19:50:39 +0100

    React: Learn useEffect Hook

--- react/my-react-project/src/components/LearnUseEffect.jsx
+++ react/my-react-project/src/components/LearnUseEffect.jsx
@@ -7,12 +7,18 @@ const LearnUseEffect = () => {
     const increaseCount = () =>{
         setCount(count + 1)
     }
+
+const generateRandomNum = () =>{
+    const randomNum = Math.floor(Math.random() * 100);
+    console.log(randomNum);
+}
   return (
     <>
         <h1>Count: {count}</h1>
         <button onClick={increaseCount}>Increase Count</button>
         <hr />
         <h1>Random Number: {randomNum}</h1>
+        <button onClick={generateRandomNum}>Generate Number</button>
     </>
   )
 }
