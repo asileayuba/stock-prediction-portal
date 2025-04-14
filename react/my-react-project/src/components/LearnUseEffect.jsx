1c90cde7b886b3e610ef15e4558f431fdd0a9f37
Asile Ayuba <asileayuba@gmail.com>
2025-04-14 19:44:16 +0100

    React: Learn useEffect Hook

--- react/my-react-project/src/components/LearnUseEffect.jsx
+++ react/my-react-project/src/components/LearnUseEffect.jsx
@@ -2,9 +2,14 @@ import {useState} from 'react'
 
 const LearnUseEffect = () => {
     const [count, setCount] = useState(0)
+
+    const increaseCount = () =>{
+        setCount(count + 1)
+    }
   return (
     <>
         <h1>Count: {count}</h1>
+        <button onClick={increaseCount}>Increase Count</button>
     </>
   )
 }
