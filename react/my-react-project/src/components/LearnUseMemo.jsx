84cccc3d0092903eaad156be0e99d5aebddae930
Asile Ayuba <asileayuba@gmail.com>
2025-04-15 18:35:36 +0100

    React: Learn useMemo Hook

--- react/my-react-project/src/components/LearnUseMemo.jsx
+++ react/my-react-project/src/components/LearnUseMemo.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LearnuseMemo = () => {
+    const [count, setCount] = useState(0)
   return (
     <div>LearnuseMemo</div>
   )
