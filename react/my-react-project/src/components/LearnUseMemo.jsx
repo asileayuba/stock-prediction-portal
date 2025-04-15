f9c3ac09b6e4d75c2cfa45ab7206d6e0217968ac
Asile Ayuba <asileayuba@gmail.com>
2025-04-15 19:07:00 +0100

    React: Learn useMemo Hook

--- react/my-react-project/src/components/LearnUseMemo.jsx
+++ react/my-react-project/src/components/LearnUseMemo.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 const LearnuseMemo = () => {
     const [count, setCount] = useState(0)
+    const [number, setNumber] = useState(10000000000)
 
     const increaseCount = () =>{
-        setCount(count + 1)
+        if (count === 10){
+            setNumber(9999999999)
+        }
+        setCount(count + 1);
     }
+
+    // const sumOfNumbers = () =>{
+    //     let sum = 0;
+    //     for(let i=1; i<number; i++){
+    //         sum += i;
+    //     }
+    //     return sum;
+    // }
+
+    const sumOfNumbers = useMemo(()=>{
+        let sum = 0;
+        for(let i=1; i<number; i++){
+            return sum;
+        }
+    }, [number])
+
+    console.log(`Sum of numbers from 1 to ${number}:`, sumOfNumbers);
+
   return (
     <>
         <h1>Count: {count}</h1>
