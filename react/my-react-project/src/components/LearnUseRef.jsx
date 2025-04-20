445c5a1f76743c6739ac0d95367856c041e19dc0
Asile Ayuba <asileayuba@gmail.com>
2025-04-20 18:54:22 +0100

    React: Learn useRef

--- react/my-react-project/src/components/LearnUseRef.jsx
+++ react/my-react-project/src/components/LearnUseRef.jsx
@@ -5,7 +5,7 @@ const LearnUseRef = () => {
   return (
     <>
         <h1>Learn useRef</h1>
-        <input type="text" value={name} />
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
     </>
   )
 }
