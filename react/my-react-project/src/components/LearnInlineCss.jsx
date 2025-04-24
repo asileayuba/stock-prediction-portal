1e0661223ff660f9627ac38e2d7b70eb62ce90b7
Asile Ayuba <asileayuba@gmail.com>
2025-04-24 18:39:33 +0100

    React: Internal CSS

--- react/my-react-project/src/components/LearnInlineCss.jsx
+++ react/my-react-project/src/components/LearnInlineCss.jsx
@@ -3,20 +3,37 @@ import React from 'react'
 const LearnInlineCss = () => {
     const style = {
         container: {
-            backgroundColor: 'red',
+            backgroundColor: 'white',
             width: '100px',
             height: '100px'
         },
         h2Text: {
             fontSize: '40px',
-            color: '#00ff000',
+            color: 'gold',
         }
     }
   return (
     <>
-        <p style={{ fontSize: '20px', fontWeight: 600, color: 'orange' }}>Hello, this is a paragraph</p>
+            <style>
+                {`
+                    .container{
+                        background-color: #00ff00
+                        height: 100px
+                        width: 100px
+                    }
+                `}
+            </style>
+    <h1>Internal CSS</h1>
+        <p style={{ fontSize: '20px', fontWeight: 600, color: 'purple' }}>Hello, this is a paragraph</p>
 
         <div style={style.container}>
+
+        </div>
+
+        <h2 style={style.h2Text}> Test H2 Element</h2>
+        <hr />
+        <h1>Internal CSS</h1>
+        <div className='container'>
 
         </div>
     </>
