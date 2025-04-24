5495e5cccd499f4dfc247068044f4226a2691b8c
Asile Ayuba <asileayuba@gmail.com>
2025-04-24 18:31:43 +0100

    React: Inline CSS

--- react/my-react-project/src/components/LearnInlineCss.jsx
+++ react/my-react-project/src/components/LearnInlineCss.jsx
@@ -8,16 +8,15 @@ const LearnInlineCss = () => {
             height: '100px'
         },
         h2Text: {
-            fontSize: '40px'
-            color: '#00ff000'
+            fontSize: '40px',
+            color: '#00ff000',
         }
     }
   return (
     <>
         <p style={{ fontSize: '20px', fontWeight: 600, color: 'orange' }}>Hello, this is a paragraph</p>
 
-        <div>
-
+        <div style={style.container}>
 
         </div>
     </>
