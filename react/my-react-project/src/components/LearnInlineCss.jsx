def23657fb35411a740083bc1b4c3511533677a8
Asile Ayuba <asileayuba@gmail.com>
2025-04-24 18:29:50 +0100

    React: Learn Map Function

--- react/my-react-project/src/components/LearnInlineCss.jsx
+++ react/my-react-project/src/components/LearnInlineCss.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const LearnInlineCss = () => {
+    const style = {
+        container: {
+            backgroundColor: 'red',
+            width: '100px',
+            height: '100px'
+        },
+        h2Text: {
+            fontSize: '40px'
+        }
+    }
   return (
     <>
-        <p style={ fontSize: '20px' }>Hello, this is a paragraph</p>
+        <p style={{ fontSize: '20px', fontWeight: 600, color: 'orange' }}>Hello, this is a paragraph</p>
+
+        <div>
+
+
+        </div>
     </>
   )
 }
