837f93ddadd1b0ff074c1a00e969afd169ae89e9
Asile Ayuba <asileayuba@gmail.com>
2025-04-24 18:55:47 +0100

    React: Externel CSS

--- react/my-react-project/src/components/LearnInlineCss.jsx
+++ react/my-react-project/src/components/LearnInlineCss.jsx
@@ -1,49 +1,55 @@
-import React from 'react'
+import React from "react";
+import "../assets/css/style.css";
 
 const LearnInlineCss = () => {
-    const style = {
-        container: {
-            backgroundColor: 'white',
-            width: '100px',
-            height: '100px'
-        },
-        h2Text: {
-            fontSize: '40px',
-            color: 'gold',
-        }
+  const style = {
+    container: {
+      backgroundColor: 'white',
+      width: '100px',
+      height: '100px'
+    },
+    h2Text: {
+      fontSize: '40px',
+      color: 'gold',
+    },
+    paragraph: {
+      fontSize: '20px',
+      fontWeight: 600,
+      color: 'purple'
     }
+  };
+
   return (
     <>
-            <style>
-                {`
-                    .container{
-                        background-color: #00ff00;
-                        height: 100px;
-                        width: 100px;
-                    }
-                `}
-            </style>
-    <h1>Internal CSS</h1>
-        <p style={{ fontSize: '20px', fontWeight: 600, color: 'purple' }}>Hello, this is a paragraph</p>
+      {/* Inline CSS */}
+      <h1>Inline CSS</h1>
+      <p style={style.paragraph}>Hello, this is a paragraph</p>
+      <div style={style.container}></div>
+      <h2 style={style.h2Text}>Test H2 Element</h2>
 
-        <div style={style.container}>
+      <hr />
 
-        </div>
+      {/* Internal CSS */}
+      <h1>Internal CSS</h1>
+      <style>
+        {`
+          .container {
+            background-color: #00ff00;
+            height: 100px;
+            width: 100px;
+          }
+        `}
+      </style>
+      <div className='container'></div>
 
-        <h2 style={style.h2Text}> Test H2 Element</h2>
-        <hr />
-        <h1>Internal CSS</h1>
-        <div className='container'>
+      <hr />
 
-        </div>
-        <hr />
-        <h1>External CSS</h1>
-        <div className='yellowbox'>
-
-        </div>
-        <h2 className='yellowtext'>Yellow Text</h2>
+      {/* External CSS */}
+      <h1>External CSS</h1>
+      <div className='yellowbox'></div>
+      <h2 className='yellowtext'>Yellow Text</h2>
     </>
-  )
-}
+  );
+};
 
-export default LearnInlineCss
+export default LearnInlineCss;
