7b56b0b4c7f0f9dba24ffecd0299d3890942d199
Asile Ayuba <asileayuba@gmail.com>
2025-04-28 18:11:23 +0100

    HomePage React

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from './Button'
 
 const Main = () => {
   return (
@@ -7,7 +8,7 @@ const Main = () => {
             <div className='p-5 text-center bg-light-dark rounded'>
                 <h1 className='text-light'>Stock Prediction Portal</h1>
                 <p className='text-light'>The Stock Prediction Portal leverages advanced Machine Learning models to forecast future stock movements, utilizing key indicators such as the 100-day and 200-day moving averages. Gain data-driven insights to support smarter and more strategic investment decisions.</p>
-                <a className='btn btn-info' href="">Login</a>
+                < Button text="Login" class="btn-outline-info" />
             </div>
         </div>
     </>
