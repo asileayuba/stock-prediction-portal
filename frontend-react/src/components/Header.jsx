63c285b1da441a398857a39fbdb9004aa8bcf278
Asile Ayuba <asileayuba@gmail.com>
2025-04-28 17:27:45 +0100

    Frontend build

---
+++ frontend-react/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <nav className='navbar container'>
+            <a className='navbar-brand text-light' href=''>Stock Prediction Portal</a>
+        </nav>
+    </>
+  )
+}
+
+export default Header
