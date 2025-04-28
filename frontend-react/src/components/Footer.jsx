788f597c2cb76b601a8efbf13f14f0595996dca5
Asile Ayuba <asileayuba@gmail.com>
2025-04-28 17:55:40 +0100

    Frontend Build: Button Component

--- frontend-react/src/components/Footer.jsx
+++ frontend-react/src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-light'>Footer</div>
+    <>
+        <footer className='footer py-3 my-3'>
+            <hr className='border-bottom' />
+            <p className='text-light text-center'>&copy; Asile 2025</p>
+        </footer>
+    </>
   )
 }
 
--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div className='text-light'>Main</div>
+    <>
+        <div className='container'>
+            <div className='p-5 text-center bg-light-dark rounded'>
+                <h1 className='text-light'>Stock Prediction Portal</h1>
+                <p className='text-light'>The Stock Prediction Portal leverages advanced Machine Learning models to forecast future stock movements, utilizing key indicators such as the 100-day and 200-day moving averages. Gain data-driven insights to support smarter and more strategic investment decisions.</p>
+                <a className='btn btn-info' href="">Login</a>
+            </div>
+        </div>
+    </>
   )
 }
 
