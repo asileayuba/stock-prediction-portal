636087acdf83cf712b2a6caeabd707fec9625984
Asile Ayuba <asileayuba@gmail.com>
2025-04-29 18:58:58 +0100

    User Serializer

--- backend-drf/accounts/serializers.py
+++ backend-drf/accounts/serializers.py
@@ -8,5 +8,14 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password']
         
-    def create():
+    def create(self, validated_data):
+        # User.objects.create = save the password in a plain text
+        # User.objects.create_user = automatically hash the password
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password']
+        )
+        # user = User.objects.create_user(**validated_data)
+        return user
         
