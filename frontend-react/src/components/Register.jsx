6001078952329ac0d6761b733f92ddfeb05113ec
Asile Ayuba <asileayuba@gmail.com>
2025-04-30 19:11:47 +0100

    Design Register Page

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -5,13 +5,17 @@ const Register = () => {
     <>
         <div className='container'>
             <div className='row justify-content-center'>
-                <div className="col-md-6"></div>
-                    <h3 className='text-light text-center'> Create an Account</h3>
-                    <form action="">
-                        <input type="text" className='form-control' placeholder='Enter username' />
-                    </form>
-            </div>
+                <div className="col-md-6 bg-light-dark p-5 rounded">
+                    <h3 className='text-light text-center mb-4'> Register for an Account </h3>
 
+                    <form>
+                        <input type="text" className='form-control mb-3' placeholder='Enter your username' />
+                        <input type="email" className='form-control mb-3' placeholder='Enter your email address' />
+                        <input type="password" className='form-control mb-5' placeholder='Create a secure password' />
+                        <button type='submit' className='btn btn-info d-block mx-auto' >Create Account</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </>
   )
