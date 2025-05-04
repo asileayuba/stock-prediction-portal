39151b4b27c2e242a2b0a3c3274b07fc9dfb9f1c
Asile Ayuba <asileayuba@gmail.com>
2025-05-04 12:05:48 +0100

    AuthContext

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import { useContext } from 'react'
 import Button from './Button'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
+
 
 const Header = () => {
+  const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
   return (
     <>
         <nav className='navbar container pt-3 pb-3 align-items-start'>
             <Link className='navbar-brand text-light' to="/">Stock Prediction Portal</Link>
 
             <div>
-                < Button text="Login" class="btn btn-outline-info" url="/login" />
-                &nbsp;
-                < Button text="Register" class="btn-info" url="/register" />
+              { isLoggedIn ? (
+                <button className='btn btn-danger'>Logout</button>
+              ): (
+                  <>
+                  < Button text="Login" class="btn btn-outline-info" url="/login" />
+                     &nbsp;
+                  < Button text="Register" class="btn-info" url="/register" />
+                  </>
+              )}
+                
             </div>
         </nav>
     </>
