b8a8b5e2aeb95d612747954157855281fb23782a
Asile Ayuba <asileayuba@gmail.com>
2025-05-04 12:28:15 +0100

    Logout Functionality

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,11 +1,20 @@
 import { useContext } from 'react'
 import Button from './Button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../AuthProvider'
 
 
 const Header = () => {
   const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    setIsLoggedIn(false)
+    console.log('Logged out');
+    navigate('/login')
+  }
   return (
     <>
         <nav className='navbar container pt-3 pb-3 align-items-start'>
@@ -13,7 +22,7 @@ const Header = () => {
 
             <div>
               { isLoggedIn ? (
-                <button className='btn btn-danger'>Logout</button>
+                <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
               ): (
                   <>
                   < Button text="Login" class="btn btn-outline-info" url="/login" />
