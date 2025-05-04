d2fa2551995575efa535dee42d6c3847fd1cde64
Asile Ayuba <asileayuba@gmail.com>
2025-05-04 11:17:08 +0100

    Login API Request

--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
   return (
-    <div className='text-light container'>Login</div>
+        <>
+            <div className='container'>
+                <div className='row justify-content-center'>
+                    <div className="col-md-6 bg-light-dark p-5 rounded">
+                        <h3 className='text-light text-center mb-4'> Login Into Your Account </h3>
+    
+                        <form>
+                            <div className="mb-3">
+                                <input type="text" className='form-control' placeholder='Enter your username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                            </div> 
+                            <div className="mb-3">
+                                <input type="password" className='form-control' placeholder='Create a secure password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            
+                            </div>
+                            
+                            {loading ? (
+                                <button type='submit' className='btn btn-info d-block mx-auto' disabled > < FontAwesomeIcon icon={faSpinner} spin /> Logging in... </button>
+                            ) : (
+                                <button type='submit' className='btn btn-info d-block mx-auto' >Login</button>
+                            )}
+                              
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
   )
 }
 
