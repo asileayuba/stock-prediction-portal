b76b4264130c48072b2a46973b71fb5a7f86331d
Asile Ayuba <asileayuba@gmail.com>
2025-05-07 15:43:35 +0100

    Dashboard Component

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
             <Route path='/' element={<Main/>} />
             <Route path='/register' element={<Register/>} />
             <Route path='/login' element={<Login/>} />
-            <Route path='/dashboard' element={< Dashboard/>} ></Route>
+            <Route path='/dashboard' element={< Dashboard/>} />
           </Routes>
         < Footer/>  
       </BrowserRouter>
--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
             <div className='p-5 text-center bg-light-dark rounded'>
                 <h1 className='text-light'>Stock Prediction Portal</h1>
                 <p className='text-light'>The Stock Prediction Portal leverages advanced Machine Learning models to forecast future stock movements, utilizing key indicators such as the 100-day and 200-day moving averages. Gain data-driven insights to support smarter and more strategic investment decisions.</p>
-                < Button text="Explore Now" class="btn-outline-info" />
+                < Button text="Explore Now" class="btn-outline-info" url="/dashboard" />
             </div>
         </div>  
         
