6505831201f9bb1a86e823c08769d52ad97ab940
Asile Ayuba <asileayuba@gmail.com>
2025-05-07 15:53:42 +0100

    Accessing Protected Endpoint from React

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Dashboard = () => {
+
+    useEffect(() => {
+        const fetchProtectedData = () =>{
+            try{
+
+            }catch(error){
+                
+            }
+        }
+    })
   return (
     <div className='text-light container'>Dashboard</div>
   )
