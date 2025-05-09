70080847586e02359558ed7cc691c4792c891c0e
Asile Ayuba <asileayuba@gmail.com>
2025-05-09 17:57:20 +0100

    Private Route

---
+++ frontend-react/src/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react"
+import { AuthContext } from "./AuthProvider"
+
+const PrivateRoute = () => {
+  return (
+    <div>PrivateRoute</div>
+  )
+}
+
+export default PrivateRoute
