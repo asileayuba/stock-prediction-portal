fc8d5833ce2a178733ce8634a1f37f335ee9eec3
Asile Ayuba <asileayuba@gmail.com>
2025-05-27 21:08:10 +0100

    Creating Serializer, View & URL

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -9,5 +9,5 @@ class StockPredictionAPIView(APIView):
     def post(self, request):
         serializer = StockPredictionSerializer(data=request.data)
         if serializer.is_valid():
-            ticker = serializer.validate_data['ticker']
-            return Response({'status': 'success'})
+            ticker = serializer.validated_data['ticker']
+            return Response({'status': 'success', 'ticker': ticker})
