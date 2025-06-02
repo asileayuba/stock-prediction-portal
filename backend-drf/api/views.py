c56224e88567c1d387d81e8a814b8c60263afa97
Asile Ayuba <asileayuba@gmail.com>
2025-06-02 22:36:36 +0100

    Display Final Prediction

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -105,9 +105,20 @@ class StockPredictionAPIView(APIView):
             # print('y_test=>', y_test)
             
             # Plot the final prediction
+            plt.switch_backend('AGG')
+            plt.figure(figsize=(12, 5))
+            plt.plot(y_test, 'b', label="Original Price")
+            plt.plot(y_predicted, 'r', label='Predicted Price')
+            plt.title(f"Final Prediction for {ticker}")
+            plt.xlabel("Days")
+            plt.ylabel("Price")
+            plt.legend()
+            plot_img_path = f"{ticker}_final_prediction.png"
+            plot_prediction = save_plot(plot_img_path)
             
             return Response({'status': 'success', 
                              'plot_img': plot_img,
                              'plot_100_dma': plot_100_dma,
                              'plot_200_dma': plot_200_dma,
+                             'plot_prediction': plot_prediction,
                              })
