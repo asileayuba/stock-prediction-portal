6f810fabe0afb9ee0ba173ffe62bb967436b6154
Asile Ayuba <asileayuba@gmail.com>
2025-06-02 22:27:20 +0100

    Making Predictions

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -11,7 +11,8 @@ from datetime import datetime
 import os
 from django.conf import settings
 from .utils import save_plot
-
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import load_model
 
 
 class StockPredictionAPIView(APIView):
@@ -73,6 +74,37 @@ class StockPredictionAPIView(APIView):
             # splitting data in Training and Testing dataset
             data_training = pd.DataFrame(df.Close[0:int(len(df)*0.7)])
             data_testing = pd.DataFrame(df.Close[int(len(df)*0.7): int(len(df))])
+            
+            # Scaling down the data between 0 and 1
+            scaler = MinMaxScaler(feature_range=(0,1))
+            
+            # Load ML Model
+            model = load_model('stock_prediction_model.keras')
+            
+            # Preparing Test Data
+            past_100_days = data_training.tail(100)
+            final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+            input_data = scaler.fit_transform(final_df)
+            
+            x_test = []
+            y_test = []
+
+            for i in range(100, input_data.shape[0]):
+                x_test.append(input_data[i-100: i])
+                y_test.append(input_data[i, 0])
+            x_test, y_test = np.array(x_test), np.array(y_test)
+            
+            # Making Predictions
+            y_predicted = model.predict(x_test)
+            
+            # Revert the scaled prices to original price
+            y_predicted = scaler.inverse_transform(y_predicted.reshape(-1, 1)).flatten()
+            y_test = scaler.inverse_transform(y_test.reshape(-1, 1)).flatten()
+            
+            # print('y_prediction=>', y_predicted)
+            # print('y_test=>', y_test)
+            
+            # Plot the final prediction
             
             return Response({'status': 'success', 
                              'plot_img': plot_img,
